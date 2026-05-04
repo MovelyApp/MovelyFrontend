709069d32b77438b18b4e0c38906e4567fe0a442
Carolina Amorim <carolinaoa1@al.insper.edu.com>
2026-05-04 19:26:41 -0300

    dashboard, navbar e header

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/dashboard.tsx"),
+    // route("groups", "routes/groups.tsx"),
+    // route("register-activity", "routes/register-activity.tsx"),
+    // route("profile", "routes/profile.tsx"),
+] satisfies RouteConfig;
